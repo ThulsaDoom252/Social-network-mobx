74ba3e1e5e6551d3341d4ef42b0dc277ca7891be
ThulsaDoom252 <xenolm252@gmail.com>
2023-10-14 15:36:49 +0300

    Updated Readme

--- src/hoc/authHoc.tsx
+++ src/hoc/authHoc.tsx
@@ -7,16 +7,11 @@ interface AuthHocProps {
     isLogged: boolean;
 }
 
-// Create an AuthHoc Higher-Order Component (HOC) function
 const authHoc = <P extends AuthHocProps>(Component: React.ComponentType<P>) => {
-    // Return a new component
     return (props: P) => {
-        // Check if the user is logged in
         if (props.isLogged) {
-            // If logged in, render the provided Component with its props
             return <Component {...props} />;
         } else {
-            // If not logged in, redirect to the authentication route
             return <>
                 <Navigate to={authRoute} />.
             </>
